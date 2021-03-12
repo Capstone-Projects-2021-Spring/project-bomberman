5ede4458089a13d69cd2fa719273b97383f7588b
Phuc <40041728+thuc753951@users.noreply.github.com>
2021-03-12 12:07:21 -0500

    SpriteSheet
    added some sprite single player methods

--- bomberman-java-master/bomberman/src/main/java/MenuState.java
+++ bomberman-java-master/bomberman/src/main/java/MenuState.java
@@ -1,3 +1,6 @@
+
+import java.awt.Graphics2D;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,25 @@
  *
  * @author Thuc Luong's PC
  */
-public class MenuState {
+public class MenuState extends GameStateSingle{
+
+    public MenuState(GameStateManager game) {
+        super(game);
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void input(keyhandler key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void render(Graphics2D buffer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
